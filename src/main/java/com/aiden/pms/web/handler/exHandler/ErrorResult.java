dbf86f5a328673c6ff025998ca1a6553c686e88b
Ordinary-Person-Jang <ks.jang6955@gmail.com>
2026-01-15 23:07:18 +0900

    에러메세지 구조 변경 - 서버 화면 메세지 일치 후 메세지타입 추가 반환

--- src/main/java/com/aiden/pms/web/handler/exHandler/ErrorResult.java
+++ src/main/java/com/aiden/pms/web/handler/exHandler/ErrorResult.java
@@ -8,4 +8,12 @@ import lombok.Data;
 public class ErrorResult {
     private String code;
     private String message;
+    private String type;
+
+    public ErrorResult() {}
+
+    public ErrorResult(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
 }
